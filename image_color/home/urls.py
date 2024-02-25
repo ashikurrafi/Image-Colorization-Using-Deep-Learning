de79afd0b8b228762055baca65049c86028da43c
Amio84 <amiocse84@gmail.com>
2024-02-26 00:29:01 +0600

    other files are updated

--- image_color/home/urls.py
+++ image_color/home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='home'),
-    path('', views.colorize_image, name='colorize_image'),
+    # path('', views.colorize_image, name='colorize_image'),
+    path('upload/', views.upload_image, name='upload_image'),
+    path('result/', views.colorize_image, name='colorize_image'),
+    path('inspect-hdf5/', views.inspect_hdf5, name='inspect_hdf5'),
 ]
--- image_color/image_color/settings.py
+++ image_color/image_color/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-qcrn(1vvb^0y0@1x^dc#b%9k_1!@d&bvw-6tz69=)2maxz##t@
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'image_color.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -127,7 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # static files
 STATICFILES_DIRS = [
@@ -142,3 +135,13 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / "media"
 
 MEDIA_URL = "/media/"
+
+# # Define the base directory of the Django project
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# # Define the path to the model file
+# MODEL_FILE_PATH = os.path.join(BASE_DIR, 'models', 'model.h5')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Define the directory where the model file is located
+MODEL_ROOT = os.path.join(BASE_DIR, 'home', 'models')
